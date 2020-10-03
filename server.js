e5b6ca33ecb4b3d1c879b69815c918b88b7df2c9
Thedyingdutchman <71300765+Thedyingdutchman@users.noreply.github.com>
2020-10-03 09:33:48 -0400

    tweaked some code

--- server.js
+++ server.js
@@ -10,40 +10,6 @@ let rawdata = fs.readFileSync('budget-data.json');
 let myBudget = JSON.parse(rawdata);
 console.log(myBudget);
 
-
-/*const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title:  'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-    {
-        title:  'Gas',
-        budget: 50
-    },
-    {
-        title:  'Utilities',
-        budget: 75
-    },
-    {
-        title:  'Wireless',
-        budget: 65
-    },
-    {
-        title:  'Misc',
-        budget: 150
-    },
-]
-}; */
-
 app.get('/hello' , (req, res) => {
     res.send('Hello World ');
 });
